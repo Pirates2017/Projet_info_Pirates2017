c7768dbaf4dc77e9a24fbe621334d754000d63d7
Zizou97 <elise.vanhecke97@gmail.com>
2017-04-15 08:58:18 +0200

    Update GameObject.java
    juste ajout de quelques propriétés pour l'ajout d'une image à chaque objet + getteurs/setteurs

--- GameObject.java
+++ GameObject.java
@@ -4,11 +4,15 @@ public abstract class GameObject {
 	protected int posX;
 	protected int posY;
 	protected int color;
+	protected int health; //durée de vie
+	protected int width, height;
 	
-	public GameObject(int X, int Y, int color){
+	public GameObject(int X, int Y, int color, int width, int heigth){
 		this.posX = X;
 		this.posY = Y;
 		this.color = color;
+		this.width = width;
+		this.height = height;
 	}
 	
 	public int getPosX(){
@@ -19,10 +23,45 @@ public abstract class GameObject {
 		return this.posY;
 	}
 	
+	
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	
 	public int getColor(){
 		return this.color;
 	}
-	
+	public void setColor(int color) {
+		this.color = color;
+	}
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	public boolean isAtPosition(int x, int y){
 		return this.posX == x && this.posY == y;
 	}
