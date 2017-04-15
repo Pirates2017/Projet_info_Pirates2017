f1141e606da12e302a7a34b6dc8530f9822fa9f0
Zizou97 <elise.vanhecke97@gmail.com>
2017-04-15 09:09:16 +0200

    Update GameObject.java
    de nouveau, même chose que pour tous les autres objets

--- GameObject.java
+++ GameObject.java
@@ -4,15 +4,35 @@ public abstract class GameObject {
 	protected int posX;
 	protected int posY;
 	protected int color;
+	protected int health; //durée de vie
+	protected int width, height;
 	
 	public GameObject(int X, int Y, int color){
 		this.posX = X;
 		this.posY = Y;
 		this.color = color;
+		this.width = width;
+		this.height = height;
 	}
 	
 	public int getPosX(){
 		return this.posX;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
 	}
 	
 	public int getPosY(){
